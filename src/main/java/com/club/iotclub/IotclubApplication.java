fd8fb26fcbc3de9d634ee908c29940635cb83fd4
dulinchao <780446681@qq.com>
2020-04-09 15:58:00 +0800

    第一次提交

---
+++ src/main/java/com/club/iotclub/IotclubApplication.java
@@ -0,0 +1,13 @@
+package com.club.iotclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/club/iotclub/IotclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.iotclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
